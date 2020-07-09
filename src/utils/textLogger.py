8e957d76a561a658a66c98d6914cf7561e087595
Renan Moreira Gomes <renangomes.es@gmail.com>
2020-07-09 12:13:44 -0300

    log machine

---
+++ src/utils/textLogger.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from pytz import timezone
+
+default_path = 'logs/'
+
+def logMsg(msg):
+    F = open(default_path + getDailyFilename(),'a')
+    F.write('[' + getFormattedTime() + ']\n')
+    F.write('LOG: ' + msg + '\n\n')
+    F.close()
+
+def getDailyFilename():
+    time = datetime.now(timezone('America/Sao_Paulo'))
+    try:
+        return time.astimezone(timezone('America/Sao_Paulo')).strftime('%d-%m-%Y')
+    except:
+        return None
+
+def getFormattedTime():
+    time = datetime.now(timezone('America/Sao_Paulo'))
+    try:
+        return time.astimezone(timezone('America/Sao_Paulo')).strftime('%d-%b-%Y (%H:%M:%S)')
+    except:
+        return None
